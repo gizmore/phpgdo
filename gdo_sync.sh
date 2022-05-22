69aa0d25e8d5e2f4a9235fff07b637d8446fa26c
gizmore <gizmore@wechall.net>
2022-05-22 11:51:16 +0200

    tea time

--- gdo_sync.sh
+++ gdo_sync.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 CORE="$(dirname "$0")"
 
-if [ "$#" -eq  "2" ]
+if [ "$2" -eq  "all" ]
     then echo "1. git push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git push" \;
-    else echo "2. git commit & push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am $@ && git push" \;
+    else echo "2. git commit & push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am '$*' && git push" \;
 fi
