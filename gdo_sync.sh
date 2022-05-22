f50250d0e438680476d451708f97c88f1e0d8bad
gizmore <gizmore@wechall.net>
2022-05-22 11:53:49 +0200

    tea time

--- gdo_sync.sh
+++ gdo_sync.sh
@@ -3,8 +3,11 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 CORE="$(dirname "$0")"
+message="'$*'"
+
+echo "Sync with $message"
 
 if [ "$2" -eq  "all" ]
     then echo "1. git push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git push" \;
-    else echo "2. git commit & push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am '$*' && git push" \;
+    else echo "2. git commit & push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am $message && git push" \;
 fi
