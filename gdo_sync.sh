c2e9b08e74132617866c24b73072891a79da94e2
gizmore <gizmore@wechall.net>
2022-05-22 11:59:12 +0200

    'tea time'

--- gdo_sync.sh
+++ gdo_sync.sh
@@ -7,7 +7,7 @@ message="'$*'"
 
 echo "GDOv7 sync.sh: Sync message: $message"
 
-if [ "'$1'" -eq  "all" ]
+if [ "$message" == "all" ]
     then echo "1. DO only push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git push" \;
-    else echo "2. DO git commit & push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am $message && git push" \;
+    else echo "2. DO git commit & push all repos" && find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am \"$message\" && git push" \;
 fi
