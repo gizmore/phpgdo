62f515cce29d47ca8bf11a55e4b72ceacbd51de2
gizmore <gizmore@wechall.net>
2022-05-28 09:55:49 +0200

    '7.0.0-alpha-6'

--- gdo_adm.php
+++ gdo_adm.php
@@ -565,7 +565,7 @@ elseif ( ($argv[1] === 'provide') || ($argv[1] === 'provide_all') || ($argv[1] =
     		$cd = count($deps);
     		foreach ($deps as $dep)
     		{
-    			if ($module = $loader->getModule($dep))
+    			if ($module = $loader->getModule($dep, true))
     			{
     				$moreDeps = $module->getDependencies();
     			}
