9282093ee56c31e3522e86fee9b33b40346df3c7
gizmore <gizmore@wechall.net>
2022-09-25 09:52:13 +0200

    '7.0.1-r1607'

--- gdo_update.sh
+++ gdo_update.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-# THREADS: number of parallel executing processes
+# THREADS: number of parallel executing  processes
 # default: THREADS=15
 #  - THREADS=0 (unlimited)
 THREADS=15
@@ -17,10 +17,11 @@ fi
 
 echo "Updating all repos in $THREADS parallel threads."
 
-find . -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> ../../temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> ../../temp/git_pull_\$OUT && git submodule update --recursive --remote &>> ../../temp/git_pull_\$OUT  ; cat ../../temp/git_pull_\$OUT && rm ../../temp/git_pull_\$OUT "
+find . -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> /tmp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> /tmp/git_pull_\$OUT && git submodule update --recursive --remote &>> /tmp/git_pull_\$OUT  ; cat /tmp/git_pull_\$OUT && rm /tmp/git_pull_\$OUT "
 
-echo "Triggering 'gdo_adm.sh update'."
-bash gdo_adm.sh update
+echo "Triggering 'gdoadm.sh update'."
+bash gdoadm.sh update
 
-echo "Triggering 'gdo_yarn.sh'."
+echo "Triggering 'gdo_yarn.sh' and 'gdo_bower.sh'."
 bash gdo_yarn.sh
+bash gdo_bower.sh
