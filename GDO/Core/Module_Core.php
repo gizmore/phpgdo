d4911fa53bd35dcc56b2487b6bf63f1d353b88fd
gizmore <gizmore@wechall.net>
2022-10-07 07:25:20 +0200

    '7.0.1-r1650 German Lang Pack - II'

--- GDO/Core/Module_Core.php
+++ GDO/Core/Module_Core.php
@@ -31,7 +31,7 @@ use GDO\Language\Module_Language;
 final class Module_Core extends GDO_Module
 {
 	const GDO_VERSION = '7.0.1';
-	const GDO_REVISION = '7.0.1-r1649';
+	const GDO_REVISION = '7.0.1-r1650';
 	const GDO_CODENAME = 'Garlic-Gremlin';
 	
 	##############
