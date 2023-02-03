e4541d1383492cecd494ebfec77ecee1b71d8ea5
gizmore <gizmore@wechall.net>
2023-02-03 06:14:27 +0100

    '7.0.1-r1728 update and config fix'

--- gdo_update.sh
+++ gdo_update.sh
@@ -19,10 +19,10 @@ echo "Resetting sourcecode to factory defaults for preprocessor."
 bash gdo_reset.sh
 
 echo "Updating all core modules and submodules in the phpgdo repo."
-find ./ -maxdepth 1 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && git submodule each git pull >> temp/git_pull_\$OUT && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> temp/git_pull_\$OUT && git submodule update --recursive --remote &>> temp/git_pull_\$OUT  ; cat temp/git_pull_\$OUT && rm temp/git_pull_\$OUT "
+find ./ -maxdepth 1 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && git submodule foreach git pull >> temp/git_pull_\$OUT && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> temp/git_pull_\$OUT && git submodule update --recursive --remote &>> temp/git_pull_\$OUT  ; cat temp/git_pull_\$OUT && rm temp/git_pull_\$OUT "
 
 echo "Updating all extension modules and submodules in $THREADS parallel threads."
-find ./GDO -maxdepth 2 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> ../../temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> ../../temp/git_pull_\$OUT && git submodule each git pull >> ../../temp/git_pull_\$OUT && git submodule update --recursive --remote &>> ../../temp/git_pull_\$OUT  ; cat ../../temp/git_pull_\$OUT && rm ../../temp/git_pull_\$OUT "
+find ./GDO -maxdepth 2 -type d -iname '.git' -print0 | xargs $XARGS_OPTIONS bash -c "cd \"{}\"/../ && OUT=\"\$(echo \"{}\" | cut -f 3 -d '/')\" && echo -e \"-----------------------------\nupdating repo [ \\\"\$(pwd)\\\" ]:\" >> ../../temp/git_pull_\$OUT && LANG=en_GB LC_ALL=en_GB git pull &>> ../../temp/git_pull_\$OUT && git submodule foreach git pull >> ../../temp/git_pull_\$OUT && git submodule update --recursive --remote &>> ../../temp/git_pull_\$OUT  ; cat ../../temp/git_pull_\$OUT && rm ../../temp/git_pull_\$OUT "
 
 cd "$(dirname "$0")"
 
