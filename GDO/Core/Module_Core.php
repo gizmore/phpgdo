9ad1fc3a53504b85ddb140abbb2f78c6bd23ff30
gizmore <gizmore@wechall.net>
2023-06-29 20:47:14 +0200

    '7.0.3-r1803 markdown.tohtml'

--- GDO/Core/Module_Core.php
+++ GDO/Core/Module_Core.php
@@ -32,7 +32,7 @@ final class Module_Core extends GDO_Module
 {
 
 	final public const GDO_VERSION = '7.0.3';
-	final public const GDO_REVISION = '7.0.3-r1802';
+	final public const GDO_REVISION = '7.0.3-r1803';
 
 	final public const GDO_CODENAME = 'Jewish Jester';
 
