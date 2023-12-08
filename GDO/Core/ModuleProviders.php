0bc630d6818ffa157c66f21ecdf7ae61f0da0b22
gizmore <gizmore@wechall.net>
2023-12-08 13:45:43 +0100

    'ESL'

--- GDO/Core/ModuleProviders.php
+++ GDO/Core/ModuleProviders.php
@@ -209,7 +209,7 @@ final class ModuleProviders
 'DoubleAccounts' => [],
 'Download' => ['Category', 'File', 'Payment', 'Votes'],
 'DSGVO' => ['Session'],
-'EdwardSnowdenLand' => ['Account', 'Bootstrap5Theme', 'IP2Country', 'FontAwesome', 'Invite', 'JQueryAutocomplete', 'Mail', 'Register', 'Votes'],
+'EdwardSnowdenLand' => ['Account', 'Avatar', 'Bootstrap5Theme', 'IP2Country', 'File', 'FontAwesome', 'Invite', 'JQueryAutocomplete', 'Mail', 'Register', 'Votes'],
 'Facebook' => [],
 'Favicon' => [],
 'FFMpeg' => ['CLI'],
