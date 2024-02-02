d310d644e4c034b9b9115249cc39db90369c52a6
gizmore <gizmore@wechall.net>
2024-02-02 13:54:46 +0100

    'net'

---
+++ GDO/Net/Method/MyIP.php
@@ -0,0 +1,16 @@
+<?php
+namespace GDO\Net\Method;
+
+use GDO\Core\GDT;
+use GDO\Core\Method;
+use GDO\Net\GDT_IP;
+
+final class MyIP extends Method
+{
+
+    public function execute(): GDT
+    {
+        return GDT_IP::make('your_ip')->useCurrent();
+    }
+    
+}
