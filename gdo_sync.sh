a9460e228ab8baadb4823037eae27a8af7a212be
gizmore <gizmore@wechall.net>
2026-07-23 10:27:25 +0200

    'attach and update submodules'

--- gdo_sync.sh
+++ gdo_sync.sh
@@ -8,7 +8,7 @@ echo "GDOv7 sync.sh: Sync message: $message"
 
 echo "Updating core submodules."
 git submodule foreach git reset --hard
-git submodule foreach --recursive git pull
+git submodule foreach --recursive 'if branch=$(git symbolic-ref --quiet --short HEAD); then git pull --ff-only; else branch=$(git symbolic-ref --quiet --short refs/remotes/origin/HEAD) && branch=${branch#origin/} && git checkout "$branch" && git pull --ff-only; fi'
 echo
 
 echo "Are you sure?"
@@ -21,4 +21,4 @@ php provider_update.php
 echo "Syncing repositories..."
 echo "Do: git commit & push all repos"
 sleep 1
-find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git add -A . && git commit -am \"$message\" && git push" \;
+find . -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && git submodule foreach --recursive 'if branch=\$(git symbolic-ref --quiet --short HEAD); then git pull --ff-only; else branch=\$(git symbolic-ref --quiet --short refs/remotes/origin/HEAD) && branch=\${branch#origin/} && git checkout \$branch && git pull --ff-only; fi' && git add -A . && git commit -am \"$message\" && git push" \;
