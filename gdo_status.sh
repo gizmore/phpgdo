e9e988199595db8bb6f01258deec67efc1bb530f
gizmore <gizmore@wechall.net>
2026-08-20 02:27:39 +0200

    status start marker

--- gdo_status.sh
+++ gdo_status.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")"
 CORE="$(dirname "$0")"
 
 clear
+echo "===GDO-STATUS==="
 find . -maxdepth 3 -iname ".git" -type d -exec sh -c "cd $CORE && cd {} && cd .. && pwd && LANG=en_GB LC_ALL=en_GB git status && git submodule foreach git status" \;
